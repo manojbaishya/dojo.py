bd7d8684761b146740cfc96a0d3621041321f916
Manoj Baishya <28330014+manojbaishya@users.noreply.github.com>
2025-05-25 12:52:20 +0530

    Add a math playground

--- dojo.py
+++ dojo.py
@@ -1,9 +1,11 @@
+from maths.sine import samplePlot
 from ostep.ostep import general_os_functions
 
 
 def main():
     print("Hello from dojo-py!")
     general_os_functions()
+    samplePlot()
 
 
 if __name__ == "__main__":
---
+++ maths/sine.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def samplePlot():
+
+    x = np.linspace(0, 2 * np.pi, 100)
+    y = np.sin(x)
+
+    plt.plot(x, y)
+    plt.title("Sine Function")
+    plt.xlabel("x (radians)")
+    plt.ylabel("sin(x)")
+    plt.grid()
+    plt.show()
